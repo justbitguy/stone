0f23b8622728d04d31d7ae97c5b16abb4c11c6a6
justbitguy <justbitguy@gmail.com>
2016-08-18 13:46:56 +0800

    firebase notification.

--- app/src/main/java/com/just/stone/activity/ImageShowActivity.java
+++ app/src/main/java/com/just/stone/activity/ImageShowActivity.java
@@ -36,11 +36,6 @@ public class ImageShowActivity extends Activity {
         findViewById(R.id.tv_pick_image).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent();
-//                intent.setType("image/*");
-//                intent.setAction(Intent.ACTION_GET_CONTENT);
-//                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-
                 Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
                 getIntent.setType("image/*");
 
