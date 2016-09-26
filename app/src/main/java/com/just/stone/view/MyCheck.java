f8cdd54dec47d0e5dbaa6d4bc5d43f673e92b80b
justbitguy <justbitguy@gmail.com>
2016-09-26 19:22:50 +0800

    custom checkbox.

---
+++ app/src/main/java/com/just/stone/view/MyCheck.java
@@ -0,0 +1,49 @@
+package com.just.stone.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.just.stone.R;
+import com.just.stone.util.LogUtil;
+
+/**
+ * Created by zhangjinwei on 2016/9/26.
+ */
+public class MyCheck extends View {
+    boolean mIsChecked = false;
+    Context context;
+    public MyCheck(Context context){
+        super(context);
+        init(context, null);
+    }
+
+    public MyCheck(Context context, AttributeSet attrs){
+        super(context, attrs);
+        init(context, null);
+    }
+
+    public void init(Context context, AttributeSet attrs){
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.MyCheck,
+                0, 0
+        );
+
+        try {
+            mIsChecked = a.getBoolean(R.styleable.MyCheck_isChecked, false);
+        } catch (Exception e){
+            LogUtil.error(e);
+        }
+
+        setBackgroundResource(mIsChecked ? R.drawable.ic_checkbox_on : R.drawable.ic_checkbox_off);
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIsChecked = !mIsChecked;
+                setBackgroundResource(mIsChecked ? R.drawable.ic_checkbox_on : R.drawable.ic_checkbox_off);
+            }
+        });
+    }
+}
